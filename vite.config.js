c1ec6aee2de8ab4e004fecddea1eb378ad110186
BossCorleone888 <carter.j.1969.masa@gmail.com>
2025-09-09 15:08:56 +0900

    build for pages

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  base: './',            // ← ここがポイント（相対パスで出力）
-  build: { outDir: 'docs' } // ← GitHub Pages の /docs 公開に合わせる
-})
