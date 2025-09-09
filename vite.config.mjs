9314cdc16a7a58f99747d13aa0cf4fd6ea0cf27e
BossCorleone888 <carter.j.1969.masa@gmail.com>
2025-09-09 16:57:21 +0900

    deploy: latest build

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,21 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/RoR-Web-site/",   // ← リポ名に合わせる（大文字小文字も）
-  build: { outDir: "docs" },// ← docs公開方式なら
+  base: "/RoR-Web-site/",
+  build: {
+    outDir: "docs",
+    // ⚠️ これを追加
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ["vue"],
+          firebase: ["firebase/app", "firebase/firestore", "firebase/auth"],
+          markdown: ["markdown-it"],
+        },
+      },
+    },
+    // （警告のしきい値を上げるなら）
+    chunkSizeWarningLimit: 1200,
+  },
   plugins: [vue()],
 });
