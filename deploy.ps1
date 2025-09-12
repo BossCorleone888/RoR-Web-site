3e3a28b6c09ed0be36debe1215b6cdf9a29a595e
BossCorleone888 <carter.j.1969.masa@gmail.com>
2025-09-12 13:21:53 +0900

    fix: firebase config (env + fallback)

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,6 @@
 cd "C:\Users\m_ohira\Desktop\RoR\RoR-web-site"
+if (Test-Path .\node_modules\.vite) { rm -r -fo .\node_modules\.vite }
+if (Test-Path .\docs) { rm -r -fo .\docs }
 npm run build
 git add .
 git commit -m "auto deploy"
